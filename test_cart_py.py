9ce5ad50c38e01851ecee018980d31d04dd0a303
ibrahimatasoy03 <ibrahimatasoy2003@gmail.com>
2026-04-16 10:33:14 +0300

    Buggy "Cart.py" and Test Codes "test_cart_py.py"

---
+++ test_cart_py.py
@@ -0,0 +1,233 @@
+import unittest
+from cart import ShoppingCart
+
+class test_add_item_function(unittest.TestCase):
+    def test_add_item_normal_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        self.assertEqual(cart._items["apple"], {"price": 1.0, "quantity": 1})
+
+    def test_add_item_edge_cases_negative_price(self):
+        cart = ShoppingCart()
+        cart.add_item("pear", -1.0, 1)
+        self.assertEqual(cart._items["pear"], {"price": -1.0, "quantity": 1})
+
+    def test_add_item_edge_case_zero_price(self):
+        cart = ShoppingCart()
+        cart.add_item("banana", 0.0, 1)
+        self.assertEqual(cart._items["banana"], {"price": 0.0, "quantity": 1})
+
+    def test_add_item_edge_cases_negative_quantity(self):
+        cart = ShoppingCart()
+        cart.add_item("grape", 1.0, -1)
+        self.assertEqual(cart._items["grape"], {"price": 1.0, "quantity": -1})
+
+    def test_add_item_edge_cases_zero_quantity(self):
+        cart = ShoppingCart()
+        cart.add_item("orange", 1.0, 0)
+        self.assertEqual(cart._items["orange"], {"price": 1.0, "quantity": 0})
+
+    def test_add_item_cumulative_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.add_item("apple", 1.0, 2)
+        self.assertEqual(cart._items["apple"], {"price": 1.0, "quantity": 3})
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_remove_item_function(unittest.TestCase):
+    def test_remove_item_normal_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.remove_item("apple")
+        self.assertEqual(cart._items, {})
+
+    def test_remove_item_edge_case_item_not_in_cart(self):
+        cart = ShoppingCart()
+        cart.remove_item("apple")
+        self.assertEqual(cart._items, {})
+
+    def test_remove_item_edge_case_empty_cart(self):
+        cart = ShoppingCart()
+        cart.remove_item("apple")
+        self.assertEqual(cart._items, {})
+
+    def test_remove_item_cumulative_case_remove_twice(self):
+        cart = ShoppingCart()
+        cart.remove_item("apple")
+        cart.remove_item("apple")
+        self.assertEqual(cart._items, {})
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_apply_discount_function(unittest.TestCase):
+    def test_apply_discount_normal_case_percentage(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 100)
+        cart.apply_discount("SAVE10")
+        self.assertEqual(cart._discount, {"type": "percent", "value": 10, "min_order": 0.0})
+
+    def test_apply_discount_normal_case_fixed(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 100)
+        cart.apply_discount("FLAT5")
+        self.assertEqual(cart._discount, {"type": "fixed", "value": 5.0, "min_order": 30.0})
+
+    def test_apply_discount_edge_case_invalid_code(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 100)
+        cart.apply_discount("ROFLCOPTER2005_THIS_CODE_IS_INVALID")
+        self.assertEqual(cart._discount, {"type": "percent", "value": 100, "min_order": 0.0})
+
+    def test_apply_discount_edge_case_zero_oder(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 0)
+        cart.apply_discount("SAVE10")
+        self.assertEqual(cart._discount, {"type": "percent", "value": 10, "min_order": 0.0})
+
+    def test_apply_discount_cumulative_case_two_codes(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 100)
+        cart.apply_discount("SAVE10")
+        cart.apply_discount("SAVE20")
+        self.assertEqual(cart._discount, {"type": "percent", "value": 20, "min_order": 50.0})
+
+    def test_apply_discount_boundary_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 50)
+        cart.apply_discount("SAVE20")
+        self.assertEqual(cart._discount, {"type": "percent", "value": 20, "min_order": 50.0})
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_get_total_function(unittest.TestCase):
+    def test_get_total_normal_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 10)
+        cart.add_item("banana", 2.0, 4)
+        cart.get_total()
+        self.assertEqual(cart.get_total(), 18.0)
+
+    def test_get_total_normal_case_remove(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 10)
+        cart.add_item("banana", 2.0, 4)
+        cart.remove_item("banana")
+        cart.get_total()
+        self.assertEqual(cart.get_total(), 10.0)
+
+    def test_get_total_edge_case_cart_empty(self):
+        cart = ShoppingCart()
+        cart.get_total()
+        self.assertEqual(cart.get_total, 0.0)
+
+    def test_get_total_cumulative_case(self):
+        cart = ShoppingCart()
+        cart.add_item("grape", 2.0, 5)
+        cart.get_total()
+        cart.add_item("banana", 3.0, 4)
+        cart.get_total()
+        self.assertEqual(cart.get_total, 22.0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_clear_function(unittest.TestCase):
+    def test_clear_normal_case_single_item(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.clear()
+        self.assertEqual(cart._items, {})
+
+    def test_clear_normal_case_multiple_items(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.add_item("banana", 2.0, 2)
+        cart.clear()
+        self.assertEqual(cart._items, {})
+
+    def test_clear_edge_case_empty_cart(self):
+        cart = ShoppingCart()
+        cart.clear()
+        self.assertEqual(cart._items, {})
+
+    def test_clear_cumulative_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.clear()
+        cart.add_item("banana", 2.0, 2)
+        cart.clear()
+        self.assertEqual(cart._items, {})
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_get_item_count_function(unittest.TestCase):
+    def test_get_item_count_normal_case_single_item(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.get_item_count()
+        self.assertEqual(cart.get_item_count(), 1)
+
+    def test_get_item_count_normal_case_single_item_multiple(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 2)
+        cart.get_item_count()
+        self.assertEqual(cart.get_item_count(), 2)
+
+    def test_get_item_count_normal_case_multiple_items(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.add_item("banana", 2.0, 2)
+        cart.get_item_count()
+        self.assertEqual(cart.get_item_count(), 3)
+
+    def test_get_item_count_edge_case_empty_cart(self):
+        cart = ShoppingCart()
+        cart.get_item_count()
+        self.assertEqual(cart.get_item_count(), 0)
+
+    def test_get_item_count_cumulative_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.get_item_count()
+        cart.add_item("banana", 2.0, 2)
+        cart.get_item_count()
+        self.assertEqual(cart.get_item_count(), 3)
+
+if __name__ == '__main__':
+    unittest.main()
+
+class test_subtotal_function(unittest.TestCase):
+    def test_subtotal_normal_case_single_item(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.subtotal()
+        self.assertEqual(cart.subtotal(), 1.0)
+
+    def test_subtotal_normal_case_multiple_item(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.add_item("banana", 2.0, 2)
+        cart.subtotal()
+        self.assertEqual(cart.subtotal(), 5.0)
+
+    def test_subtotal_edge_case_empty_cart(self):
+        cart = ShoppingCart()
+
+        cart.subtotal()
+        self.assertEqual(cart.subtotal(), 0.0)
+
+    def test_subtotal_cumulative_case(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1.0, 1)
+        cart.subtotal()
+        cart.add_item("banana", 2.0, 2)
+        cart.subtotal()
+        self.assertEqual(cart.subtotal(), 5.0)
+
+if __name__ == '__main__':
+    unittest.main()
